9aae41925688adc6bb0b34663dcfa9ea32123058
WW71 <76815640+WW71@users.noreply.github.com>
2022-07-20 12:24:21 +1000

    Add files via upload

---
+++ RemoveUsersfromGroups.ps1
@@ -0,0 +1,10 @@
+﻿# This script is for batch removing a list of users from a list of AD groups.
+$userlist = import-csv C:\TEMP\users.csv
+$grouplist = import-csv C:\TEMP\groups.csv
+
+# The script looks for columns with the headers "account" and "grouplist".
+ForEach($group in $grouplist)
+    {
+    Get-ADGroupMember -Identity $group.grouplist | 
+    %{Remove-ADGroupMember -Identity $group.grouplist -Members $userlist.account -Confirm:$false}
+    }
